e5d67bdb4f4ec4bdee71df01c14fa1ab8507f132
Tahir Bencheikh <tbencheikh.consultant@sqi.gouv.qc.ca>
2025-02-18 10:22:24 -0500

    test

--- terraform/import_all_resources.sh
+++ terraform/import_all_resources.sh
@@ -15,7 +15,7 @@ import_resource() {
   AZURE_ID=$3
 
   echo "Vérification de l'existence de la ressource $RESOURCE_NAME de type $RESOURCE_TYPE..." | tee -a $LOG_FILE
-  if az resource show --ids $AZURE_ID &> /dev/null; then
+  if az resource show --ids "$AZURE_ID" &> /dev/null; then
     echo "La ressource existe. Importation dans Terraform..." | tee -a $LOG_FILE
     terraform import "$RESOURCE_TYPE.$RESOURCE_NAME" "$AZURE_ID" | tee -a $LOG_FILE
     if [ $? -ne 0 ]; then
@@ -28,7 +28,7 @@ import_resource() {
 }
 
 # Liste des ressources à importer
-RESOURCES=( 
+RESOURCES=(
   "azurerm_resource_group rg /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP"
   "azurerm_virtual_network vnet /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/virtualNetworks/savard-vnet"
   "azurerm_subnet subnet /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/virtualNetworks/savard-vnet/subnets/savard-subnet"
@@ -37,12 +37,13 @@ RESOURCES=(
   "azurerm_network_interface nic /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/networkInterfaces/savard-nic"
   "azurerm_windows_virtual_machine server /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/savard-server"
   "azurerm_virtual_machine_extension winrm_extension /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/savard-server/extensions/enable-winrm"
+  "azurerm_network_interface_security_group_association nic_nsg_assoc /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/networkInterfaces/savard-nic|/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/networkSecurityGroups/winrm-nsg"
 )
 
 # Importer toutes les ressources
 for RESOURCE in "${RESOURCES[@]}"; do
-  RESOURCE_TYPE=$(echo $RESOURCE | awk '{print $1}')
-  RESOURCE_NAME=$(echo $RESOURCE | awk '{print $2}')
-  AZURE_ID=$(echo $RESOURCE | awk '{print $3}')
+  RESOURCE_TYPE=$(echo "$RESOURCE" | awk '{print $1}')
+  RESOURCE_NAME=$(echo "$RESOURCE" | awk '{print $2}')
+  AZURE_ID=$(echo "$RESOURCE" | awk '{print $3}')
   import_resource "$RESOURCE_TYPE" "$RESOURCE_NAME" "$AZURE_ID"
 done
