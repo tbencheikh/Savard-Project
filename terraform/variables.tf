18f9f29181ae3b76b31a868065e98fa99fd8ba09
Tahir Bencheikh <tbencheikh.consultant@sqi.gouv.qc.ca>
2025-02-18 09:38:24 -0500

    test - TF_VAR

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,6 +2,7 @@ variable "admin_password" {
   description = "Mot de passe administrateur pour les machines virtuelles"
   type        = string
   sensitive   = true
+  default = ""
 }
 variable "subscription_id" {
   description = "ID de la souscription Azure"
