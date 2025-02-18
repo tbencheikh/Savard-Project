acee6339d812283ac58dc91636267f1757aeaac9
Tahir Bencheikh <tbencheikh.consultant@sqi.gouv.qc.ca>
2025-02-17 21:06:08 -0500

    retest

--- terraform/import_all_resources.sh
+++ terraform/import_all_resources.sh
@@ -28,21 +28,21 @@ import_resource() {
 }
 
 # Liste des ressources à importer
-RESOURCES=(
-  "azurerm_resource_group /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP"
-  "azurerm_virtual_network /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/virtualNetworks/savard-vnet"
-  "azurerm_subnet /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/virtualNetworks/savard-vnet/subnets/savard-subnet"
-  "azurerm_public_ip /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/publicIPAddresses/savard-public-ip"
-  "azurerm_network_security_group /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/networkSecurityGroups/winrm-nsg"
-  "azurerm_network_interface /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/networkInterfaces/savard-nic"
-  "azurerm_windows_virtual_machine /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/savard-server"
-  "azurerm_virtual_machine_extension /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/savard-server/extensions/enable-winrm"
+RESOURCES=( 
+  "azurerm_resource_group rg /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP"
+  "azurerm_virtual_network vnet /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/virtualNetworks/savard-vnet"
+  "azurerm_subnet subnet /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/virtualNetworks/savard-vnet/subnets/savard-subnet"
+  "azurerm_public_ip public_ip /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/publicIPAddresses/savard-public-ip"
+  "azurerm_network_security_group winrm_nsg /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/networkSecurityGroups/winrm-nsg"
+  "azurerm_network_interface nic /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/networkInterfaces/savard-nic"
+  "azurerm_windows_virtual_machine server /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/savard-server"
+  "azurerm_virtual_machine_extension winrm_extension /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Compute/virtualMachines/savard-server/extensions/enable-winrm"
 )
 
 # Importer toutes les ressources
 for RESOURCE in "${RESOURCES[@]}"; do
   RESOURCE_TYPE=$(echo $RESOURCE | awk '{print $1}')
-  AZURE_ID=$(echo $RESOURCE | awk '{print $2}')
-  RESOURCE_NAME=$(echo $RESOURCE_TYPE | awk -F'.' '{print $2}')
+  RESOURCE_NAME=$(echo $RESOURCE | awk '{print $2}')
+  AZURE_ID=$(echo $RESOURCE | awk '{print $3}')
   import_resource "$RESOURCE_TYPE" "$RESOURCE_NAME" "$AZURE_ID"
 done
